e0db93a251a9d3f722b11b5946a5847b72d36c6d
Najdi Ayoub <najdi14.ayoub@gmail.com>
2021-01-09 20:20:17 +0100

    first test

---
+++ main.py
@@ -0,0 +1,53 @@
+print ("this is my first test")
+print ("----A")
+print ("   %%%%   " * 10)
+print("*" * 10)
+
+price = 20
+rating = 4.9
+name = "Ayoub"
+is_published = False
+print(price)
+
+name = input('what is your name dude !')
+color = input('what is your favorite color !')
+
+print(name + " " + " LOVES " + color)
+birthday_year = input('Birthday Year : ')
+print(type(birthday_year))
+age = 2020 - int(birthday_year)
+print(age)
+print(type(age))
+
+weight = input("what's your weight (lbs):")
+kilo = float(weight) * 0.45
+print(kilo)
+
+email = ''' 
+Hi Charlez When can see that you love the "black color" and 
+your 28 with "98kg"
+
+thank you!
+the support Team !!
+
+'''
+print(email)
+
+cours = 'Python Course Online'
+
+other = cours[7:13]
+print(other)
+num1 = input("enter the first number ")
+num2 = input("enter the second number ")
+perc = 12
+somme = (int(num1) + int(num2) * int(perc))
+print(somme)
+
+
+
+
+
+
+
+
+
